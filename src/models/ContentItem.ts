228e4aaefb774078df3855ff9b7dee94016af397
Jeremy Cook <jcook61592@gmail.com>
2021-07-22 14:43:03 -0400

    localizedString updated on model

--- src/models/ContentItem.ts
+++ src/models/ContentItem.ts
@@ -1,6 +1,6 @@
 export const ContentItem: Object = {
   name: String,
-  localizedNames: String,
+  localizedNames: String, // Field is excluded from the response when a locale is set
   id: String,
   assetName: String,
   assetPath: String,
