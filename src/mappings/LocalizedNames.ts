336a3904ffbd06d9d1c9e4be7ec7cd439307eea8
Steven Herring <stevenmherring@gmail.com>
2021-07-21 20:26:37 -0700

    Adds Localization and Response Error mapping/enums

---
+++ src/mappings/LocalizedNames.ts
@@ -0,0 +1,21 @@
+export enum LocalizedNames {
+    'ARABIC' = 'ar-AE',
+    'GERMAN' = 'de-DE',
+    'BRITISH' = 'en-GB',
+    'ENGLISH' = 'en-US',
+    'SPANISH' = 'es-ES',
+    'MEXICAN' = 'es-MX',
+    'FRENCH' = 'fr-FR',
+    'INDONESIA' = 'id-ID',
+    'ITALY' = 'it-IT', // 400 Unknown locale
+    'JAPAN' = 'ja-JP',
+    'KOREA' = 'ko-KR',
+    'POLAND' = 'pl-PL',
+    'BRAZIL' = 'pt-BR',
+    'RUSSIA' = 'ru-RU',
+    'THAILAND' = 'th-TH',
+    'TURKEY' = 'tr-TR',
+    'VIETNAM' = 'vi-VN',
+    'CHINA' = 'zh-CN',
+    'TAIWAN' = 'zh-TW',
+}
