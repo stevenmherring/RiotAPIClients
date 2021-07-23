b497a68bbcdc74f29aa799a7cabf2d697ea11df6
Steven Herring <stevenmherring@gmail.com>
2021-07-22 17:45:57 -0700

    Adds jest, config and test script

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/", "src/mappings/", "src/models/"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
