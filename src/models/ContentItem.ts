0a55e80c592c0cf5f03766a9e82590f1b4fdccef
Jeremy Cook <jcook61592@gmail.com>
2021-07-22 21:04:59 -0400

    Content Item Model Set Up

---
+++ src/models/ContentItem.ts
@@ -0,0 +1,15 @@
+import { LocalizedNames } from "../mappings/LocalizedNames";
+
+export class ContentItem {
+  name: string;
+  id: string;
+  /**
+   * Field is excluded from the response when a locale is set
+   */
+  localizedNames?: Record<LocalizedNames, string>;
+  assetName: string;
+  /**
+   * This field is only included for maps and games modes. These values are used in the match response.
+   */
+  assetPath?: string;
+}
