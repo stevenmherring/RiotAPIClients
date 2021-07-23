d33b65faa391c61598c91551c9c8f7faea434940
JC00K <Jcook61592@gmail.com>
2021-07-22 20:55:32 -0400

    Delete ContentItem.ts

--- src/models/ContentItem.ts
+++
@@ -1,7 +0,0 @@
-export const ContentItem: Object = {
-  name: String,
-  localizedNames: String, // Field is excluded from the response when a locale is set
-  id: String,
-  assetName: String,
-  assetPath: String,
-};
