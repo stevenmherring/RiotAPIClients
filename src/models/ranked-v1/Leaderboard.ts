41f68ab62f3d1d910584faffe983af08706642da
Steven Herring <stevenmherring@gmail.com>
2021-08-01 14:40:26 -0700

    Adds ranked models (#8)

---
+++ src/models/ranked-v1/Leaderboard.ts
@@ -0,0 +1,18 @@
+import { Shard } from "../../@types/Account";
+import { Player } from "./Player";
+
+export class Leaderboard {
+  /**
+   * The shard for the given leaderboard.
+   */
+  shard: Shard;
+  /**
+   * The act id for the given leaderboard. Act ids can be found using the val-content API.
+   */
+  actId: string;
+  /**
+   * The total number of players in the leaderboard.
+   */
+  totalPlayers: number;
+  players: Player[];
+}
---
+++ src/models/ranked-v1/Player.ts
@@ -0,0 +1,19 @@
+import { PUUID } from "../../@types/Account";
+
+export class Player {
+  /**
+   * This field may be omitted if the player has been anonymized.
+   */
+  puuid: PUUID;
+  /**
+   * This field may be omitted if the player has been anonymized.
+   */
+  gameName: string;
+  /**
+   * This field may be omitted if the player has been anonymized.
+   */
+  tagLine: string;
+  leaderboardRank: number;
+  rankedRating: number;
+  numberOfWins: number;
+}
